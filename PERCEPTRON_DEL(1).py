67a0143e6d9c7aa37a7ad495033c0b80d7c599f9
Incharaaaa <incharac.ai23@bmsce.ac.in>
2025-08-19 16:31:23 +0530

    Add files via upload

---
+++ PERCEPTRON_DEL(1).py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def linear_activation(x):
+    return x
+
+def sigmoid_activation(x):
+    return 1 / (1 + np.exp(-x))
+
+def tanh_activation(x):
+    return np.tanh(x)
+
+def relu_activation(x):
+    return np.maximum(0, x)
+
+def softmax_activation(x):
+    # softmax over a vector
+    exp_vals = np.exp(x - np.max(x))  # stability trick
+    return exp_vals / np.sum(exp_vals)
+
+def perceptron_output(x, w, b, activation_func):
+    """
+    x: input
+    w: weight
+    b: bias
+    activation_func: activation function (linear, sigmoid, tanh, ReLU, etc.)
+    """
+    z = w * x + b
+    return activation_func(z)
+
+x_vals = np.linspace(-10, 10, 200)
+
+weight = 1.0
+bias = 0.0
+
+linear_outputs = perceptron_output(x_vals, weight, bias, linear_activation)
+sigmoid_outputs = perceptron_output(x_vals, weight, bias, sigmoid_activation)
+tanh_outputs = perceptron_output(x_vals, weight, bias, tanh_activation)
+relu_outputs = perceptron_output(x_vals, weight, bias, relu_activation)
+softmax_outputs = softmax_activation(x_vals)
+
+plt.figure(figsize=(12, 7))
+plt.plot(x_vals, linear_outputs, label="Linear", color='blue')
+plt.plot(x_vals, sigmoid_outputs, label="Sigmoid", color='green')
+plt.plot(x_vals, tanh_outputs, label="Tanh", color='red')
+plt.plot(x_vals, relu_outputs, label="ReLU", color='purple')
+plt.plot(x_vals, softmax_outputs, label="Softmax", color='cyan')
+plt.title("Perceptron Output with Different Activation Functions")
+plt.xlabel("Input (x)")
+plt.ylabel("Output (y)")
+plt.grid(True)
+plt.legend()
+plt.ylim(-2, 2) 
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
